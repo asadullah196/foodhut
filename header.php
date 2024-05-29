ea67a20f94b2fe8457a836157ea68650b7e1cc6b
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-29 13:24:46 +0800

    header and footer file has been added

---
+++ header.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The header of the Constra theme.
+ *
+ * This file serves as the header template for Constra WordPress theme.
+ * It contains the opening HTML document structure, <head> section, and
+ * the beginning of the <body> section.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Constra
+ * @subpackage Template
+ * @since 1.0.0
+ */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <?php if (is_singular() && pings_open( get_queried_object() ) ) : ?>
+    <?php endif; ?>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+    <meta name="generator" content="<?php echo esc_attr( 'WordPress ' . get_bloginfo( 'version' ) ); ?>">
+
+    <?php wp_head(); ?>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home" <?php body_class(); ?>>
+    
+    <!-- Navbar -->
+    <nav class="custom-navbar navbar navbar-expand-lg navbar-dark fixed-top" data-spy="affix" data-offset-top="10">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="menu.html">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#book-table">Book-Table</a>
+                </li>
+            </ul>
+            <a class="navbar-brand m-auto" href="index.html">
+                <img src="assets/imgs/logo.svg" class="brand-img" alt="">
+                <span class="brand-txt">Food Hut</span>
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="blog.html">Blog<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#testmonial">Reviews</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="components.html" class="btn btn-primary ml-xl-4">Components</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- header -->
+    <header id="home" class="header">
+        <div class="overlay text-white text-center">
+            <h1 class="display-2 font-weight-bold my-3">Food Hut</h1>
+            <h2 class="display-4 mb-5">Always fresh &amp; Delightful</h2>
+            <a class="btn btn-lg btn-primary" href="#gallary">View Our gallary</a>
+        </div>
+    </header>
