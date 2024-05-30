43fbd8eed6e75848297eab959685d19cd54837a3
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-31 02:42:16 +0800

    Little code change before I sleep

--- plugins/foodhut-widgets/widgets/hello-world.php
+++ plugins/foodhut-widgets/widgets/hello-world.php
@@ -159,7 +159,7 @@ class Hello_World extends Widget_Base {
 		<!-- header -->
 		<header id="home" class="header" style="background-image: url('<?php echo get_template_directory_uri().'/assets/imgs/main.jpg'; ?>'); background-repeat: no-repeat; background-position: center; background-size: cover; position: relative; background-color: #1f1f1f;">
 			<div class="overlay text-white text-center">
-				<h1 class="display-2 font-weight-bold my-3">Food Hut</h1>
+				<h1 class="display-2 font-weight-bold my-3">Food Hut Here</h1>
 				<h2 class="display-4 mb-5">Always fresh &amp; Delightful</h2>
 				<a class="btn btn-lg btn-primary" href="#gallary">View Our gallary</a>
 			</div>
