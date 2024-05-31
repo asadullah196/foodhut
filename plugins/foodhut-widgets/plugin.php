cdb821382e10f0630cfdc87e9b53adcbd31e6bfe
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-31 21:50:10 +0800

    Menu widget done

--- plugins/foodhut-widgets/plugin.php
+++ plugins/foodhut-widgets/plugin.php
@@ -117,12 +117,14 @@ class Foodhut_Wigets_Plugin {
 		// Its is now safe to include Widgets files
 		require_once( __DIR__ . '/widgets/main-hero.php' );
 		require_once( __DIR__ . '/widgets/main-about.php' );
+		require_once( __DIR__ . '/widgets/menu-item.php' );
 		require_once( __DIR__ . '/widgets/contact-us.php' );
 		require_once( __DIR__ . '/widgets/find-us.php' );
 
 		// Register Widgets
 		$widgets_manager->register( new Widgets\Foodhut_Main_Hero() );
 		$widgets_manager->register( new Widgets\Foodhut_Main_About() );
+		$widgets_manager->register( new Widgets\Foodhut_Menu_Item() );
 		$widgets_manager->register( new Widgets\Foodhut_Contact_Us() );
 		$widgets_manager->register( new Widgets\Foodhut_Find_Us() );
 	}
---
+++ plugins/foodhut-widgets/widgets/menu-item.php
@@ -0,0 +1,163 @@
+<?php
+namespace FoodhutWidgets\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Elementor Foodhut hero
+ *
+ * Elementor widget for Foodhut hero.
+ *
+ * @since 1.0.0
+ */
+class Foodhut_Menu_Item extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'foodhut-menu-item';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Menu Item', 'food-hut-widgets' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-bag-medium';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'foodhut-category' ];
+	}
+
+	/**
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
+	public function get_script_depends() {
+		return [ 'food-hut-widgets' ];
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function register_controls() {
+
+		$this->start_controls_section(
+			'foodhut_menu_item',
+			[
+				'label' => __( 'Content', 'food-hut-widgets' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'foodhut_bg',
+			[
+				'label' => esc_html__( 'Choose Background Image', 'food-hut-widgets' ),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+				'default' => [
+					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				],
+			]
+		);
+
+		$this->add_control(
+			'foodhut_url',
+			[
+				'label' => esc_html__( 'Add Link', 'food-hut-widgets' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'options' => [ 'url', 'is_external', 'nofollow' ],
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					// 'custom_attributes' => '',
+				],
+				'label_block' => true,
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+		?>
+
+    <!-- Single menu item  -->
+	<div class="gallary">
+	<div class="gallary-item wow fadeIn">
+        <img src="<?php echo esc_url($settings['foodhut_bg']['url']); ?>" alt="Menu Items" class="gallary-img">
+			<a href="<?php echo esc_url($settings['foodhut_url']['url']); ?>" class="gallary-overlay">
+				<i class="gallary-icon ti-plus"></i>
+			</a>
+    </div>
+	</div>
+		<?php
+	}
+}
